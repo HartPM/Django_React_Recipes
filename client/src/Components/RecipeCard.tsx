75556066c8e0e0799622edec25084c35e6e9d8be
Patrick Hart <pathart612@gmail.com>
2022-12-02 13:37:47 -0500

    GET request working

--- client/src/Components/RecipeCard.tsx
+++ client/src/Components/RecipeCard.tsx
@@ -1,6 +1,25 @@
+import * as React from 'react';
+
 function RecipeCard() {
+  const [title, setTitle] = React.useState('');
+
+  const makeAPICall = async () => {
+    try {
+      const res = await fetch('www.themealdb.com/api/json/v1/1/random.php');
+      const data = await res.json();
+      setTitle(data.meals[0].strMeal);
+    }
+    catch (e) {
+      console.log(e)
+    }
+  }
+  
+  React.useEffect(() => {
+    makeAPICall();
+  }, [])
+
   return (
-    <h1>Recipe</h1>
+    <h1>{title}</h1>
   )
 }
 
